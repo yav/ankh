92da3947a0627e3da1da989875e136f4e4681287
Iavor S. Diatchki <diatchki@galois.com>
2026-02-21 12:20:45 -0800

    Parsing for pieces

--- app/App/Piece.hs
+++ app/App/Piece.hs
@@ -1,12 +1,15 @@
 module App.Piece where
 
 import Data.Aeson qualified as JS
+import Data.Aeson ((.:))
+import Data.Aeson.Types (Parser)
+import Data.Text (Text)
 import KOI.Basics (PlayerId)
 
 -- | Game pieces
 data Piece
   = PlayerPiece PlayerId PlayerPieceType
-  | Structure StructureType
+  | Structure (Maybe PlayerId) StructureType
   deriving (Read, Show)
 
 -- | Types of player pieces
@@ -36,10 +39,7 @@ instance JS.ToJSON PlayerPieceType where
     case x of
       God -> JS.String "god"
       Soldier -> JS.String "soldier"
-      Guardian guardianType -> JS.object
-        [ "type" JS..= JS.String "guardian"
-        , "guardianType" JS..= guardianType
-        ]
+      Guardian _guardianType -> JS.String "guardian"
 
 instance JS.ToJSON StructureType where
   toJSON x =
@@ -53,9 +53,46 @@ instance JS.ToJSON Piece where
     case x of
       PlayerPiece player pieceType -> JS.object
         [ "player" JS..= player
-        , "pieceType" JS..= pieceType
+        , "kind" JS..= pieceType
         ]
-      Structure structType -> JS.object
-        [ "type" JS..= JS.String "structure"
-        , "structureType" JS..= structType
+      Structure mbPlayer structType -> JS.object
+        [ "player" JS..= mbPlayer
+        , "kind" JS..= structType
         ]
+
+instance JS.FromJSON Piece where
+  parseJSON = parsePiece JS.parseJSON
+
+-- Parsing functions
+
+parsePiece :: (JS.Value -> Parser (Maybe PlayerId)) -> JS.Value -> Parser Piece
+parsePiece parsePlayerId = JS.withObject "Piece" $ \obj -> do
+  kind <- obj .: "kind"
+  playerVal <- obj .: "player"
+  mbPlayer <- parsePlayerId playerVal
+
+  -- Try to parse as structure type first
+  case parseStructureType kind of
+    Just structType -> pure (Structure mbPlayer structType)
+    Nothing -> do
+      -- Must be a player piece type
+      pieceType <- parsePlayerPieceType kind
+      case mbPlayer of
+        Nothing -> fail "Player piece requires a non-null player field"
+        Just player -> pure (PlayerPiece player pieceType)
+
+parsePlayerPieceType :: Text -> Parser PlayerPieceType
+parsePlayerPieceType txt =
+  case txt of
+    "god" -> pure God
+    "soldier" -> pure Soldier
+    "guardian" -> pure (Guardian GuardianType)
+    _ -> fail ("Unknown player piece type: " ++ show txt)
+
+parseStructureType :: Text -> Maybe StructureType
+parseStructureType txt =
+  case txt of
+    "temple" -> Just Temple
+    "obelisk" -> Just Obelisk
+    "pyramid" -> Just Pyramid
+    _ -> Nothing
